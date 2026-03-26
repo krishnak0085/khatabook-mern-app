e6576e95ec27b404a0a186f30d01ddf38d68f157
krishnak0085 <krishna.gupta85com@gmail.com>
2026-03-26 17:22:57 +0530

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken")
+
+module.exports = (req,res,next)=>{
+
+ const token = req.headers.authorization
+
+ if(!token){
+  return res.status(401).json({msg:"No token"})
+ }
+
+ try{
+
+  const decoded = jwt.verify(token,"secretkey")
+
+  req.userId = decoded.id
+
+  next()
+
+ }catch(err){
+
+  res.status(401).json({msg:"Invalid token"})
+
+ }
+
+}
