8389066998e60b45d4b57887ff72badcb0786ccd
krishnak0085 <krishna.gupta85com@gmail.com>
2026-04-16 18:25:10 +0530

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,6 +1,10 @@
-const mongoose = require("mongoose")
-
 const transactionSchema = new mongoose.Schema({
+
+ userId:{
+  type: mongoose.Schema.Types.ObjectId,
+  ref:"User",
+  required:true
+ },
 
  customerId:{
   type: mongoose.Schema.Types.ObjectId,
@@ -27,5 +31,3 @@ const transactionSchema = new mongoose.Schema({
  }
 
 })
-
-module.exports = mongoose.model("Transaction",transactionSchema)
