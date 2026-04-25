df64ca7e025477229f65013e46867e0bf49c2a4f
krishnak0085 <krishna.gupta85com@gmail.com>
2026-04-25 20:14:59 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -36,11 +36,12 @@ router.post("/login", async (req,res)=>{
 
  if(!valid)
   return res.status(400).json({msg:"Wrong password"})
-const token = jwt.sign(
- {id:user._id},
- process.env.JWT_SECRET,
- {expiresIn:"7d"}
-)
+
+ const token = jwt.sign(
+  {id:user._id},
+  "secretkey"
+ )
+
  res.json({token})
 
 })
