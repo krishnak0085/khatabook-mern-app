26ebb5d730b47ebd42b1e5ddcc3f0fbdb67ac4d8
krishnak0085 <krishna.gupta85com@gmail.com>
2026-04-25 20:17:16 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,17 +2,15 @@ const jwt = require("jsonwebtoken")
 
 module.exports = (req,res,next)=>{
 
- const authHeader = req.headers.authorization
+ const token = req.headers.authorization
 
- if(!authHeader){
+ if(!token){
   return res.status(401).json({msg:"No token"})
  }
 
- const token = authHeader.split(" ")[1]
-
  try{
 
-  const decoded = jwt.verify(token,process.env.JWT_SECRET)
+  const decoded = jwt.verify(token,"secretkey")
 
   req.userId = decoded.id
 
@@ -21,6 +19,7 @@ module.exports = (req,res,next)=>{
  }catch(err){
 
   res.status(401).json({msg:"Invalid token"})
+
  }
 
 }
