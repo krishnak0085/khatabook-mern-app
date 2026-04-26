eed455d74b41e29225981b3be64a3f7bcb87aeee
krishnak0085 <krishna.gupta85com@gmail.com>
2026-04-26 10:19:56 +0530

    Update CustomerDetails.jsx

--- frontend/src/pages/CustomerDetails.jsx
+++ frontend/src/pages/CustomerDetails.jsx
@@ -329,7 +329,8 @@ data.sort((a,b)=>{
   return dateDiff
  }
 
-return new Date(b._id) - new Date(a._id)})
+ return b._id.localeCompare(a._id)
+})
 // take last N entries
 if(limit !== "all"){
  data = data.slice(0, Number(limit))
