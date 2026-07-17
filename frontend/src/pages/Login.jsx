0f7c6c9db7e286acbe7f54130ffe2c7eff0ee178
krishnak0085 <krishna.gupta85com@gmail.com>
2026-07-17 23:25:03 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login(){
   e.preventDefault()
 
   const res = await axios.post(
-   "https://khatabook-mern-app-production.up.railway.app/api/auth/login",
+   "https://khatabook-mern-app-azew.onrender.com/api/auth/login",
    {email,password}
   )
 
