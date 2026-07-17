6db1999c570c02eaa1b8e81eb0a9372e0b1d084e
krishnak0085 <krishna.gupta85com@gmail.com>
2026-07-17 17:36:21 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -15,7 +15,7 @@ export default function Register(){
   e.preventDefault()
 
   await axios.post(
-   "https://khatabook-mern-app.onrender.com/api/auth/register",
+   "https://khatabook-mern-app-production.up.railway.app/api/auth/register",
    {name,email,password}
   )
 
